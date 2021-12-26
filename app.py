985caecc3845421d9a51a4fb3dfc8083c4c207e8
sa100 <sergio.rudenko@gmail.com>
2021-12-26 23:36:03 +0300

    pyinstaller script added, relative path fix for onefile distr

---
+++ app.py
@@ -0,0 +1,128 @@
+import os
+import sys
+import json
+# import asyncio
+from typing import List
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def send_personal_json(self, data: json, websocket: WebSocket):
+        await websocket.send_json(data)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+    async def broadcast_json(self, data: json):
+        for connection in self.active_connections:
+            await connection.send_json(data)
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception as e:
+        print('Exception:', e)
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+app = FastAPI()
+app.mount("/vue-app", StaticFiles(directory=resource_path('vue-app')), name='static')
+
+manager = ConnectionManager()
+
+html = ''
+with open(resource_path('index.html'), 'r') as file:
+    html = file.read()
+
+
+@app.get('/')
+async def get():
+    return RedirectResponse('vue-app/index.html')
+
+
+@app.get('/static/html')
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket('/ws/echo')
+async def websocket_echo(websocket: WebSocket):
+    await manager.connect(websocket)
+    await manager.send_personal_message(f'Hello there!', websocket)
+    try:
+        while True:
+            message = await websocket.receive_text()
+            await manager.send_personal_message(f'You wrote "{message}"', websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
+
+@app.websocket('/ws/api/v1')
+async def websocket_api(websocket: WebSocket):
+    await manager.connect(websocket)
+    await manager.send_personal_json({
+        'hello': 'there!',
+    }, websocket)
+    # await manager.send_personal_message(json.dumps({
+    #    'connected': True,
+    # }), websocket)
+    try:
+        while True:
+            message = await websocket.receive_json()
+            print(message)
+            await manager.send_personal_json({
+                'request': message,
+                'response': 'ok'
+            }, websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
+
+@app.websocket('/ws/{client_id}')
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    await manager.broadcast(f'Client #{client_id} connected to the chat')
+    try:
+        while True:
+            message = await websocket.receive_text()
+            await manager.send_personal_message(f'You wrote: "{message}"', websocket)
+            await manager.broadcast(f'Client #{client_id} wrote: "{message}"')
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f'Client #{client_id} left the chat')
+
+# async def periodic_message():
+#     """ Periodic task """
+#     counter = 0;
+#     while True:
+#         counter += 1
+#         await manager.broadcast_json({
+#             'counter': counter,
+#             'text': 'Hello, world!'
+#         })
+#         await asyncio.sleep(5)
+#
+#
+# loop = asyncio.get_event_loop()
+# task = loop.create_task(periodic_message())
--- main.py
+++ main.py
@@ -1,115 +1,15 @@
-import json
-import asyncio
-from typing import List
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
+import os
+import sys
+import uvicorn
 
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def send_personal_json(self, data: json, websocket: WebSocket):
-        await websocket.send_json(data)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
-    async def broadcast_json(self, data: json):
-        for connection in self.active_connections:
-            await connection.send_json(data)
-
-
-app = FastAPI()
-app.mount("/vue-app", StaticFiles(directory='vue-app'), name='static')
-
-manager = ConnectionManager()
-
-html = ''
-with open('index.html', 'r') as file:
-    html = file.read()
-
-
-@app.get('/')
-async def get():
-    return RedirectResponse('vue-app/index.html')
-
-
-@app.get('/static/html')
-async def get():
-    return HTMLResponse(html)
-
-
-@app.websocket('/ws/echo')
-async def websocket_echo(websocket: WebSocket):
-    await manager.connect(websocket)
-    await manager.send_personal_message(f'Hello there!', websocket)
+if __name__ == '__main__':
     try:
-        while True:
-            message = await websocket.receive_text()
-            await manager.send_personal_message(f'You wrote "{message}"', websocket)
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
+        print(sys._MEIPASS)
+        print(os.listdir(sys._MEIPASS))
+    except Exception as e:
+        print('Exception:', e)
+        pass
 
-
-@app.websocket('/ws/api/v1')
-async def websocket_api(websocket: WebSocket):
-    await manager.connect(websocket)
-    await manager.send_personal_json({
-        'hello': 'there!',
-    }, websocket)
-    # await manager.send_personal_message(json.dumps({
-    #    'connected': True,
-    # }), websocket)
-    try:
-        while True:
-            message = await websocket.receive_json()
-            print(message)
-            await manager.send_personal_json({
-                'request': message,
-                'response': 'ok'
-            }, websocket)
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-
-
-@app.websocket('/ws/{client_id}')
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    await manager.broadcast(f'Client #{client_id} connected to the chat')
-    try:
-        while True:
-            message = await websocket.receive_text()
-            await manager.send_personal_message(f'You wrote: "{message}"', websocket)
-            await manager.broadcast(f'Client #{client_id} wrote: "{message}"')
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f'Client #{client_id} left the chat')
-
-
-async def periodic_message():
-    """ Periodic task """
-    counter = 0;
-    while True:
-        counter += 1
-        await manager.broadcast_json({
-            'counter': counter,
-            'text': 'Hello, world!'
-        })
-        await asyncio.sleep(5)
-
-
-loop = asyncio.get_event_loop()
-task = loop.create_task(periodic_message())
+    ''' START '''
+    uvicorn.run('app:app', host='0.0.0.0', port=8888, reload=False)
+    print('exiting...')
