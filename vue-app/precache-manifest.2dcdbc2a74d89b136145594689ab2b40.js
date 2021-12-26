2f5a98f176d532dcdeb6789dc82400f4a9906179
sa100 <sergio.rudenko@gmail.com>
2021-12-26 21:15:27 +0300

    main:app and vue-app as static

---
+++ vue-app/precache-manifest.2dcdbc2a74d89b136145594689ab2b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdc8e8ef3c63a6ad92a",
+    "url": "/vue-app/css/app.6768d50e.css"
+  },
+  {
+    "revision": "d49a79df95f16ff94317c7ca74ce8bc9",
+    "url": "/vue-app/index.html"
+  },
+  {
+    "revision": "29a8d3391bb77f066478",
+    "url": "/vue-app/js/about.d8e20a04.js"
+  },
+  {
+    "revision": "ebdc8e8ef3c63a6ad92a",
+    "url": "/vue-app/js/app.2015f902.js"
+  },
+  {
+    "revision": "bf267525086865ba332d",
+    "url": "/vue-app/js/chunk-vendors.e691f250.js"
+  },
+  {
+    "revision": "b6de7814a65bd7d5846a8598fb6fc992",
+    "url": "/vue-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app/robots.txt"
+  }
+]);
